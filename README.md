c42346177a0f1d9923b6bdf4a52695854589447a
Abdullah Saeed <bnbshrbconect@gmail.com>
2025-08-23 10:31:26 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This analyze tweets and predict the sentiment of the tweet, [Tweets dataste](https://www.kaggle.com/datasets/kazanova/sentiment140).
+
+This project will be a web application, by entering tweet will return a negative and netrual comments, and how many of comments and the percent of negative comments.
+
+**System:**
+ - When open the page, will be a input for the tweet's url
+ - After submit the url, the backend get the tweet data (Username, name, logo, tweet, date) will shows like in X, the backend will send tweet and comments with (comment data, prediction, positive and negative words in the comment).
---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, jsonify
+import re
+import numpy as np
+import joblib
+
+model = joblib.load("./machine_learning/tweet_sentiment/estimators/model.joblib")
+vectorizer = joblib.load("./machine_learning/tweet_sentiment/estimators/vectorizer.joblib")
+
+feature_names = vectorizer.get_feature_names_out()
+coefs = model.coef_[0]
+intercept = model.intercept_[0]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process_text', methods=['POST'])
+def process_text():
+    data = request.get_json()
+    text = data.get('text', '')
+
+    X_text = vectorizer.transform([text])
+
+    negative_proba = model.predict_proba(X_text)[0][0]
+
+    tfidf_values = X_text.toarray()[0]
+
+    contributions = (-intercept * (negative_proba * 2 - 1)) + (tfidf_values * coefs)
+
+    # Only keep features present in the text
+    nonzero_indices = np.where(tfidf_values != 0)[0]
+
+    result = []
+    # Map feature -> contribution
+    feature_to_weight = {feature_names[i]: contributions[i] for i in nonzero_indices}
+
+    # Split text into tokens for ordering
+    tokens = re.findall(r'\b\w+\b', text)  
+    
+    # Collect unigrams + bigrams in order
+    words = []
+    i = 0
+    while i < len(tokens):
+        unigram = tokens[i]
+        if i < len(tokens) - 1:
+            bigram = tokens[i] + " " + tokens[i+1]
+            if bigram.lower() in feature_to_weight:
+                words.append(bigram)
+                i += 2
+                continue
+        if unigram.lower() in feature_to_weight:
+            words.append(unigram)
+        else:
+            words.append(unigram)
+        i +=1 
+    words_weight = [-feature_to_weight.get(token.lower(), 0) for token in words]
+    result = list(zip(words, words_weight))
+
+    return jsonify({'words': result, "negative_percent": negative_proba})
+
+if __name__ == '__main__':
+    app.run(debug=True)
